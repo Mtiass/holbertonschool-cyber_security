83909270b6e2b3a1c1b7bc4a8682bb3a683003ba
Mtiass <mdlsproff@gmail.com>
2024-08-05 13:37:51 -0400

    delete t0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-whois "$1" | awk -F': ' '/^Registrant / {sub(/^Registrant /, "Registrant "); print $1", "$2} /^Admin / {sub(/^Admin /, "Admin "); print $1", "$2} /^Tech / {sub(/^Tech /, "Tech "); print $1", "$2} ' > "$1.csv"
