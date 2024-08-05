1ae3d36248d2b15530d8025a3d982e99b229cc5f
Mtiass <mdlsproff@gmail.com>
2024-08-05 13:26:44 -0400

    Task 0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/Registrant / {print "Registrant", $2} /Admin / {print "Admin", $2} /Tech / {print "Tech", $2}' | awk -F', ' '{print $1","$2}' > "$1.csv"
+whois "$1" | awk -F': ' '/Registrant / || /Admin / || /Tech / {print $1, $2}' > "$1.csv"
