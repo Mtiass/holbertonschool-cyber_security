ec3966832eaaae7df3746d719c8698b68eb5f32a
Mtiass <mdlsproff@gmail.com>
2024-08-05 13:09:12 -0400

    Task 0 and README

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1"
+whois "$1" | awk -F': ' '/Registrant / || /Admin / || /Tech / {print $1, $2}' | awk '{gsub(/^Registrant /, "Registrant,"); gsub(/^Admin /, "Admin,"); gsub(/^Tech /, "Tech,"); print}' > "$1.csv"
