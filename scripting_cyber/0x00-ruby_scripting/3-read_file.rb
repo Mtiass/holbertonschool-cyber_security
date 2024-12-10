ab059688d68d8c9644c4007bbc86f4465a466355
Mtiass <mdlsproff@gmail.com>
2024-12-10 09:58:51 -0500

    task 3

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -5,7 +5,7 @@ def count_user_ids(path)
   file_cont = File.read(path)
   info = JSON.parse(file_cont)
   u_id_count = 0
-  data.each do |item|
+  info.each do |item|
     u_id_count += 1 if item['userId']
   end
   return u_id_count
