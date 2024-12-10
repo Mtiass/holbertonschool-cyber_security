fcdf8b84a2b900ca2f6fdff93c81557b58d41aeb
Mtiass <mdlsproff@gmail.com>
2024-12-10 10:39:15 -0500

    task 9

--- scripting_cyber/0x00-ruby_scripting/9-download_file.rb
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -3,16 +3,15 @@ require 'open-uri'
 
 if ARGV.length != 2
   puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
+  exit
 end
 
-else
-  url = ARGV[0]
-  local_file_path = ARGV[1]
-  file = open(url)
-  content = file.read
-  file.close
+url = ARGV[0]
+local_file_path = ARGV[1]
+file = open(url)
+content = file.read
+file.close
   
-  local_file = File.open(local_file_path, 'wb')
-  local_file.write(content)
-  local_file.close
-end
+local_file = File.open(local_file_path, 'wb')
+local_file.write(content)
+local_file.close
