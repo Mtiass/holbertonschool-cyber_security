5531ff0c9300597b398fd0e4e88e2366dc2e1b80
Mtiass <mdlsproff@gmail.com>
2024-12-10 10:43:53 -0500

    task 9

--- scripting_cyber/0x00-ruby_scripting/9-download_file.rb
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -1,6 +1,7 @@
 #!/bin/usr/env ruby
 require 'open-uri'
 require 'uri'
+require 'fileutils'
 
 if ARGV.length != 2
   puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
@@ -12,6 +13,8 @@ local_file_path = ARGV[1]
 file = URI.open(url)
 content = file.read
 file.close
+
+dir = File.dirname(local_file_path)
   
 local_file = File.open(local_file_path, 'wb')
 local_file.write(content)
