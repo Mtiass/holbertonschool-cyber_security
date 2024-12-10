4c2b0f91d7ec876f26503aeb4f8dc8c6f8eed3d4
Mtiass <mdlsproff@gmail.com>
2024-12-10 09:01:34 -0500

    task 1

--- scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 class HelloWorld
   def initialize
-    @message = "Hello World!"
+    @message = "Hello, World!"
   end
 
   def print_hello
-    puts "#{@meassage}"
+    puts @meassage
   end
 end
