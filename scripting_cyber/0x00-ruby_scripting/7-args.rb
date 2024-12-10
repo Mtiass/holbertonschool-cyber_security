8e8ac8ee0b933409b388ba03a590b591cd7d6096
Mtiass <mdlsproff@gmail.com>
2024-12-10 10:21:53 -0500

    task 7

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -3,7 +3,7 @@ def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
-    ARGV.each do |arg, index|
+    ARGV.each_with_index do |arg, index|
       puts "#{index + 1}. #{arg}"
     end
   end
