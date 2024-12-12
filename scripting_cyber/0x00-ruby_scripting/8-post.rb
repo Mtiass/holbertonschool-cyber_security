4ab12d9a190eb74b1d72b047539a0e4e725bfbd7
Mtiass <mdlsproff@gmail.com>
2024-12-12 14:32:38 -0500

    task 8

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'net/http'
+require 'uri'
+
+def post_request(url, body_params)
+  
+  uri = URI.parse(url)
+
+  request = Net::HTTP::Post.new(uri)
+
+  request.body = body_params.to_json if body_params
+
+  response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    http.request(request)
+  end
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
+end
