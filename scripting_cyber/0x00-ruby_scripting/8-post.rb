2e6d9dc4d446a8693686ced0378ee22e2dc64a48
Mtiass <mdlsproff@gmail.com>
2024-12-12 15:17:39 -0500

    task 8

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -11,11 +11,14 @@ def post_request(url, body_params)
 
   request.body = body_params.to_json if body_params
 
+  request["Content-Type"] = "application/json"
+
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
     http.request(request)
   end
 
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
-  puts JSON.pretty_generate(JSON.parse(response.body))
+  body = JSON.parse(response.body)
+  puts JSON.pretty_generate(body)
 end
